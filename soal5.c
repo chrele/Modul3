6655974e58db9a6ca685023dc21f0806719ba6c8
ulfaza <ulfatuzzahroh2@gmail.com>
2017-11-02 19:37:08 +0700

    fix yaa

--- soal5.c
+++ soal5.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 
 pthread_t tid[100];
-int arr[100];
+char *arr[100];
 int x;
 
-void* faktorial (void *arg){
-    int i, j;
-    pthread_t id=pthread_self();
+void* count (void *arg){
+    int i;
+    pthread_t id = pthread_self();
     for(i=0; i<x; i++){
         if(pthread_equal(id,tid[i])){
-	    unsigned long long int fak=1;
-    	    for(j=1; j<=arr[i]; j++) 
-		fak*=j;
-	    printf("%d!=%llu  ",arr[i],fak);
+    	    FILE *fp;
+	    int sum=0;
+	    char read[1000];
+	    fp=fopen("Novel.txt", "r");
+	    while(!feof(fp)){
+	        fscanf(fp, "%s", read);
+		if(strstr(read, arr[i])!=NULL)
+		    sum++;
+	    }
+	    fclose(fp);
+	    printf("jumlah %s=%d ",arr[i],sum);
 	}
     }
 }
@@ -26,12 +34,11 @@ int main(int argc, char **argv)
     x=0;
     while(1){
        if(argv[x+1] == NULL) break;
-       num = atoi(argv[x+1]);
-       arr[x] = num;
+       arr[x]=argv[x+1];
        x++;
     } 
     for(i=0; i<x; i++){
-       pthread_create (&tid[i],NULL,&faktorial,NULL);
+       pthread_create (&tid[i],NULL,&count,NULL);
     }
     for(j=0; j<x; j++){
        pthread_join(tid[j],NULL);
